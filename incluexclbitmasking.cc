100ddc628b1fe05536c665b64cdfbdec65f44fd9
pashmash372 <abhilashsivasanker@gmail.com>
2020-07-17 19:06:13 +0530

    Intial upload of the project

---
+++ incluexclbitmasking.cc
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h" 
+using namespace std; 
+
+int main(){
+	int a[]={2,3,5,7};
+	int n=sizeof(a)/sizeof(int);
+	int ans=0;
+	int no=999;
+
+	for(int i=1;i<(1<<n);i++){
+		int mask=i;
+		int bits=__builtin_popcount(mask);
+		int temp=1;
+		int pos=0;
+
+		while(mask>0){
+			int lastbit=(mask&1);
+			if(lastbit){
+				temp*=a[pos];		
+			}
+			mask>>=1;
+			pos++;
+		}
+
+		if(bits&1)
+			ans+=no/temp;
+		else
+			ans-=no/temp;		
+	}
+	cout<<ans;
+	return 0;
+}
