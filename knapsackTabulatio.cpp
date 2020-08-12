68d7377b8a839f25831cc2b2d77473dad7f68dce
pashmash372 <abhilashsivasanker@gmail.com>
2020-08-13 00:13:19 +0530

    knapsack in in bottom-up manner

---
+++ knapsackTabulatio.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapSack(int W, int wt[], int val[],
+             int n)
+{
+    int t[n + 1][W + 1];
+
+    for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                t[i][j] = 0;
+            }
+            else if (wt[i - 1] <= W)
+            {
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]],
+                              t[i - 1][j]);
+            }
+            else
+            {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+    return t[n][W];
+}
+
+int main() 
+{ 
+    int val[] = { 60, 100, 120 }; 
+    int wt[] = { 10, 20, 30 }; 
+    int W = 50; 
+    int n = sizeof(val) / sizeof(val[0]); 
+    printf("%d", knapSack(W, wt, val, n)); 
+    return 0; 
+} 
