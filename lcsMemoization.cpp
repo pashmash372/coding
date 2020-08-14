3a80c4932376fee2f556e221f201ed4ee7cab5b6
pashmash372 <abhilashsivasanker@gmail.com>
2020-08-14 15:09:27 +0530

    Longest Common Subsequence Memoization technique

---
+++ lcsMemoization.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop_0(x, n) for (int x = 0; x < n; x++)
+#define loop(x, i, n) for (int x = i; x < n; x++)
+
+int t[1001][1001];
+
+int lcs(char X[], char Y[], int m, int n)
+{
+    
+    if (m == 0 || n == 0)
+        return 0;        
+    if(t[m][n]!=-1)
+        return t[m][n];
+    else if (X[m - 1] == Y[n - 1])
+    {
+        return 1 + lcs(X, Y, m - 1, n - 1);
+    }
+    else
+    {
+        return max(
+            lcs(X, Y, m - 1, n),
+            lcs(X, Y, m, n - 1));
+    }
+}
+
+int main()
+{
+    char X[] = "AGGTAB";
+    char Y[] = "GXTXAYB";
+
+    int m = strlen(X);
+    int n = strlen(Y);
+
+    memset(t,-1,sizeof(t));
+
+    cout << "Length of LCS is " << lcs(X, Y, m, n);
+
+    return 0;
+}
