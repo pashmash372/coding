53303174dbc942bd3d0cb8498185199bebc641d9
pashmash372 <abhilashsivasanker@gmail.com>
2020-08-28 13:15:00 +0530

    next greater to right

---
+++ nextGreaterToRight.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+stack<int> s;
+void ngr(int arr[], int size)
+{
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (s.size() == 0)
+        {
+            v.push_back(-1);
+        }
+        else if (s.size() > 0 && s.top() > arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else if (s.size() > 0 && s.top() <= arr[i])
+        {
+            while (s.size() > 0 && s.top() <= arr[i])
+            {
+                s.pop();
+            }
+            if (s.size() == 0)
+            {
+                v.push_back(-1);
+            }
+            else
+            {
+                v.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    }
+    reverse(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr[] = {1, 3, 0, 0, 1, 2, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    ngr(arr, size);
+
+    return 0;
+};
