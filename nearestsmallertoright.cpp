43a26336578e58c05da4e98a3e8cd576746e661d
pashmash372 <abhilashsivasanker@gmail.com>
2020-08-29 00:29:36 +0530

    nearest smaller to right

---
+++ nearestsmallertoright.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+stack<int> s;
+
+void nsr(int arr[], int size)
+{
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (s.size() == 0)
+        {
+            v.push_back(-1);
+        }
+        else if (s.size() > 0 && s.top() < arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else if (s.size() > 0 && s.top() >= arr[i])
+        {
+            while (s.size() > 0 && s.top() >= arr[i])
+            {
+                s.pop();
+            }
+            if (s.size() == 0)
+            {
+                v.push_back(-1);
+            }
+            else
+            {
+                v.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    }
+    reverse(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {4, 5, 2, 10, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    nsr(arr, size);
+    return 0;
+}
